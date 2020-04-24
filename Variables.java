4bcc4848ff44d5907d5b81a1d4d5505b52045971
Andreas Roessler <andreas.roessler@hs-esslingen.de>
2020-04-24 10:53:44 +0200

    added vars

---
+++ Variables.java
@@ -0,0 +1,14 @@
+
+// Importieren von zusätzlichen Features; hier: Scanner
+import java.util.*;
+
+public class Variables {
+    public static void main(final String[] args) {
+        // Locale.setDefault(Locale.US);
+
+        int i = 5;
+        float f = 3.1415f;
+
+        System.out.format("Zahl %d %.2f %n", i, f);
+    }
+}
